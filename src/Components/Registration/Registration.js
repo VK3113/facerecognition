6c7eb91fd4bd94975df40d6ace37421cc03cb461
VK3113 <viswanath.kaushik@gmail.com>
2020-05-13 15:42:27 -0500

    FE/API integration

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -2,7 +2,61 @@ import React from 'react';
 import Styles from './Registration.module.css';
 import Register from './registration.png';
 
-const Registration = ({ onRouteChange }) => {
+class Registration extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            regId    : '',
+            regUname : '',
+            regEmail : '',
+            regPassword: ''
+        }
+    }
+
+    onIdChange = (event) => {
+        this.setState({regId : event.target.value});
+    }
+
+    onUnameChange = (event) => {
+        this.setState({regUname : event.target.value});
+    }
+
+    onEmailChange = (event) => {
+        this.setState({regEmail : event.target.value});
+    }
+
+    onPasswordChange = (event) => {
+        this.setState({regPassword : event.target.value});
+    }
+
+    onSubmitReg = () => {
+        fetch('http://localhost:3000/register', {
+            method          : 'post',
+            headers         : {'Content-Type' : 'application/json'},
+            body : JSON.stringify({
+                id          : this.state.regId,
+                name        : this.state.regUname,
+                email       : this.state.regEmail,
+                password    : this.state.regPassword
+            })
+        })
+        // console.log(JSON.stringify({
+        //     email           : this.state.signInEmail,
+        //     password        : this.state.signInPassword
+        // }))
+        .then(response => response.json())
+        .then(data => {
+            if (data === 'registered') {
+            this.props.onRouteChange('home');
+            }
+        })
+     
+    }
+    render () {
+
+        const { onRouteChange } = this.props;
+
     return (
 
         // <!--Registration form will have one main div cointatiner with one div for error messages, form with divs for each input field and a submit button outside the form-->
@@ -21,15 +75,28 @@ const Registration = ({ onRouteChange }) => {
                     <fieldset className = {Styles.fieldset}>
                     <div className = {Styles.divall}>
                         <label  htmlFor="firstname" >First and Last name</label>
-                        <input className = {Styles.inputcls} type="text" name="firstname"  autoFocus={true} placeholder="Enter First and Last name"></input>
+                        <input 
+                        className = {Styles.inputcls} 
+                        type="text" 
+                        name="firstname"  
+                        autoFocus={true} 
+                        placeholder="Enter First and Last name"
+                        
+                        ></input>
                     </div>
                     {/* <!--<div >
                         <label htmlFor="lastname" >Lastname</label>
                         <input type="text" name="lastname"  ></input>
                     </div>--> */}
                     <div className = {Styles.divall}>
-                        <label  htmlFor="address" >Street address, City, ZIP Code</label>
-                        <input className = {Styles.inputcls} type="text" name="address"  placeholder="Enter street address, city and ZIP code" ></input>
+                        <label  htmlFor="Address" >Address</label>
+                        <input 
+                        className = {Styles.inputcls} 
+                        type="text" 
+                        name="Address"  
+                        placeholder="Enter Street, City, Zip Code" 
+                       
+                        ></input>
                     </div>
                     {/* <!--<div class="rgtrn-txt-div">
                         <label for="zipcode" class="label-style">ZIP Code</label>
@@ -66,17 +133,45 @@ const Registration = ({ onRouteChange }) => {
                         <input className = {Styles.inputchoice} type="checkbox" name="politics" value="politics" />politics
                         <input className = {Styles.inputchoice} type="checkbox" name="movies" value="movies" />Movies
                     </div>
+                    
                     <div className = {Styles.divall}>
-                        <label  htmlFor="email" >Email Id</label>
-                        <input className = {Styles.inputcls} type="email" name="email" placeholder="Enter valid email id" ></input>
+                        <label  htmlFor="Id" >User Id</label>
+                        <input 
+                        className = {Styles.inputcls} 
+                        type="text" 
+                        name="Id"  
+                        placeholder="Enter User Id" 
+                        onChange = {this.onIdChange}
+                        ></input>
                     </div>
                     <div className = {Styles.divall}> 
                         <label htmlFor="username" >Username</label>
-                        <input className = {Styles.inputcls} type="text" name="username"  placeholder="Choose a username win min.5 characters and max.9 characters" ></input>
+                        <input 
+                        className = {Styles.inputcls} 
+                        type="text" name="username"  
+                        placeholder="Choose a username win min.5 characters and max.9 characters" 
+                        onChange = {this.onUnameChange}
+                        ></input>
+                    </div>
+                    <div className = {Styles.divall}>
+                        <label  htmlFor="email" >Email Id</label>
+                        <input 
+                        className = {Styles.inputcls} 
+                        type="email" 
+                        name="email" 
+                        placeholder="Enter valid email id" 
+                        onChange = {this.onEmailChange}
+                        ></input>
                     </div>
                     <div className = {Styles.divall}>
                         <label  htmlFor="password" >Password</label>
-                        <input className = {Styles.inputcls} type="password" name="password"  placeholder="Enter password. Password requirements provided below" ></input>
+                        <input 
+                        className = {Styles.inputcls} 
+                        type="password" 
+                        name="password"  
+                        placeholder="Enter password. Password requirements provided below" 
+                        onChange = {this.onPasswordChange}
+                        ></input>
                         
                     </div>
                     <div className = {Styles.psswdReq1}>
@@ -90,14 +185,17 @@ const Registration = ({ onRouteChange }) => {
                     </div>
                     
                     <div className = {Styles.divbutton}>
-                        <input  type="submit" value="Register" ></input>
+                        <input  onClick = {this.onSubmitReg} type="submit" value="Register" ></input>
                     </div>
                     
                     </fieldset>
             </div>
             
         </div>
+    
     )
+}
+
 
 
 
--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -1,49 +1,97 @@
-import React from 'react';
+import React, { Component} from 'react';
 import Brain from '../Logo/Logo.png';
-
 import Styles from './Signin.module.css';
+import { thistle } from 'color-name';
 
 
-const Signin = ({ onRouteChange }) => {
+class Signin extends Component  {
+    constructor(props) {
+        super(props);
+        this.state = {
+            signInEmail : '',
+            signInPassword : ''
+        }
+    }
+    
+    onemailChange = (event) => {
+        this.setState({signInEmail : event.target.value})
+    }
+    
+    onpasswordChange = (event) => {
+        this.setState({signInPassword : event.target.value})
+    }
+
+    onsubmitSignIn = () => {
+        fetch('http://localhost:3000/signin', {
+            method          : 'post',
+            headers         : {'Content-Type' : 'application/json'},
+            body : JSON.stringify({
+                email       : this.state.signInEmail,
+                password    : this.state.signInPassword
+            })
+        })
+        // console.log(JSON.stringify({
+        //     email           : this.state.signInEmail,
+        //     password        : this.state.signInPassword
+        // }))
+        .then(response => response.json())
+        .then(data => {
+            if (data === 'success') {
+            this.props.onRouteChange('home');
+            }
+        })
+       
+    }
+
+
+    render () {
+        const { onRouteChange } = this.props;
     return(
         <div className = {Styles.divContainer}> {/* Container that has one div for logo and one form */}
-
             {/* Div for logo 'divlogo'     */}
             <div >
                 <img src = {Brain} alt = '' className = {Styles.logoImg}></img>
             </div>
-
             {/* form will contain div header with "Sign-In" text and prompt to register, Fieldset for email and passwdd */}
             <div>
             <div >
                 <h1 >Log In</h1>
-               
                     <span >Need an account?</span>
                     <p className = {Styles.pclass} onClick ={() => onRouteChange('Registration')} > Register</p>
-                
             </div>
             <fieldset className = {Styles.fieldset}>
                 <div>
                     <div className = {Styles.divFields} >
                         <label  className = {Styles.labelcls}>Username</label>
-                        <input type="text" name="usrname" id="usrname" autoFocus={true}  placeholder ="Enter your username" className = {Styles.inputcls}></input>
+                        <input 
+                        type="text" 
+                        name="usrname" 
+                        id="usrname" 
+                        autoFocus={true}  
+                        placeholder ="Enter your username" 
+                        className = {Styles.inputcls}
+                        onChange = {this.onemailChange}
+                        ></input>
                     </div>
                     <div  className = {Styles.divFields}>
                         <label   className = {Styles.labelcls} >Password</label>
-                        <input type="password" name="psswd" id="psswd"  placeholder="Enter password" className = {Styles.inputcls}></input>
+                        <input 
+                        type="password" 
+                        name="psswd" 
+                        id="psswd"  
+                        placeholder="Enter password" 
+                        className = {Styles.inputcls}
+                        onChange = {this.onpasswordChange}
+                        ></input>
                     </div>
                     <div className = {Styles.divFields} >
-                        <input onClick = {() => onRouteChange('home')} type="submit" value="Sign In" className = {Styles.buttoncls} ></input>
+                        <input onClick = {this.onsubmitSignIn} type="submit" value="Sign In" className = {Styles.buttoncls} ></input>
                     </div>
                 </div>
             </fieldset>
-           
              </div>
-
-         
         </div>
-        
-    )
+        )
+    }
 }
-
 export default Signin;
